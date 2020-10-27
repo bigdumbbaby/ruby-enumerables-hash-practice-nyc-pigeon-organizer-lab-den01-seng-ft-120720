6cf6356a4f84e142e502a71737f006b702e48c4e
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 17:08:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,10 @@ require "pry"
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), output_array| 
     binding.pry
-    value.each do 
+    value.each do |color_key, names| 
+      names.each do |name|
+      end
+    end
+    
+  end
 end
