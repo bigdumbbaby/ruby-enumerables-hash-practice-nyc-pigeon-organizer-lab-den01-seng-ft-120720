79b9d50dd81e7711d7337d14d3e4a94758b2a617
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 17:22:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,15 @@ require "pry"
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array| 
     binding.pry
-    value.each do |color_key, names| 
+    value.each do |inner_key, names| 
       names.each do |name|
-        if !output_array[name]
-          output_array[name] = {}
+        if !final_array[name]
+          final_array[name] = {}
         end
-        if !output_array[name][key]
-          output_array[name][key] = []
+        if !final_array[name][key]
+          final_array[name][key] = []
         end
+        final_array[name][key].push[inner_key]
       end
     end
     
